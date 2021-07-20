70d73af7af2c12c714a1d408bb35e9a33c564e26
thangemlun <thien2357@gmail.com>
2021-07-21 00:36:27 +0700

    finish traffic light project

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react' ;
 import './TrafficLight.css';
-
+import classNames from 'classnames';
 const RED = 0 ;
 const ORANGE = 1 ;
 const GREEN = 2 ;
@@ -8,32 +8,44 @@ const GREEN = 2 ;
 class TrafficLight extends Component {
     constructor() {
         super();
-        this.currentColor = RED ;
+        this.state = {
+            currentColor : GREEN 
+        };
 
         setInterval(() => {
-            this.currentColor = this.getNextColor(this.currentColor);
-            console.log(this.currentColor);
+            this.setState ({
+                currentColor : this.getNextColor(this.state.currentColor)
+            });
         },1000);
 
     }
 
     getNextColor(color){
         switch(color){
-            case RED :
+            case GREEN :
                 return ORANGE;
             case ORANGE : 
                 return RED ;
             default:
-                return RED;
+                return GREEN;
         }
     }
 
     render() {
+        const{ currentColor } = this.state;
+
+        console.log('rendering ... ' , currentColor);
         return(
             <div className="TrafficLight">
-                <div className="bulb red" ></div>
-                <div className="bulb orange" ></div>
-                <div className="bulb green" ></div>
+                <div className={classNames('bulb','red',{
+                    active: currentColor === RED  
+                })}/>
+                <div className={classNames('bulb','orange',{
+                    active: currentColor === ORANGE  
+                })} ></div>
+                <div className={classNames('bulb','green',{
+                    active: currentColor === GREEN  
+                })} ></div>
             </div>
         );
     }
